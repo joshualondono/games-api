a6c274629a1d228560d5fbf1aa570059542f3d0c
joshua londono <joshualondono@outlook.com>
2020-11-02 11:36:26 -0500

    updated

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const app = express();
 require('dotenv').config();
 
-const userRoutes = require('./routes/userRoutes');
+const gameRoutes = require('./routes/gameRoutes');
 
 const port = process.env.PORT || 3001;
 //general middleware
@@ -14,8 +14,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-//useroutes middleware
-app.use('/api/v1/users', userRoutes);
+//gameoutes middleware
+app.use('/api/v1/games', gameRoutes);
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
--- models/gamesArray.js
+++ models/gamesArray.js
@@ -1,4 +1,4 @@
-let users = [
+let games = [
   { id: '0', name: 'game1', description: 'fun', yearReleased: '123', playtime: '60' },
   { id: '1', name: 'game2', description: 'boring', yearReleased: '123', playtime: '60' },
   { id: '2', name: 'game3', description: 'cool', yearReleased: '123', playtime: '60' },
@@ -6,4 +6,4 @@ let users = [
   { id: '5', name: 'game5', description: 'sucks', yearReleased: '123', playtime: '60' }
 ];
 
-module.exports = users;
+module.exports = games;
